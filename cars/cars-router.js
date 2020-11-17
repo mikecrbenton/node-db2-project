5573837ef9f0785985325161d14bc8cf7c018dcd
mikecrbenton <mikecrbenton@hotmail.com>
2020-11-16 21:07:22 -0600

    mvp and 2 of 3 stretch completed

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -38,6 +38,48 @@ router.post("/cars", async (req, res, next) => {
 	}
 })
 
+// PUT ==================================================
+router.put("/cars/:id", async(req,res,next) => {
+   try{
+      const payload = {
+         VIN : req.body.VIN,
+         make : req.body.make,
+         model : req.body.model,
+         mileage : req.body.mileage,
+         manual : req.body.manual,
+         title_status : req.body.title_status
+      }
+
+      if(!payload.VIN || !payload.mileage || !payload.make || !payload.model   ) {
+         return res.status(400).json( {message: "Need VIN, make, model, and mileage"})
+      }
+
+      await db("car-dealer").where("id", req.params.id).update(payload)
+
+      const updatedCar = await db
+         .first("*")
+         .from("car-dealer")
+         .where("id", req.params.id)
+
+      res.json(updatedCar)
+
+   }catch(err){
+      next(err)
+   }
+} )
+
+// DELETE ===============================================
+router.delete("/cars/:id", async(req, res, next) => {
+   try{
+    // DON'T FORGET WHERE!!!  DON'T DELETE YOUR DATABASE LIKE A CHUMP
+      await db("car-dealer").where("id", req.params.id).del()
+      res.status(204).end() //204 = success empty response
+      
+   }catch(err){
+      next(err)
+   }
+})
+
 
 
 module.exports = router
